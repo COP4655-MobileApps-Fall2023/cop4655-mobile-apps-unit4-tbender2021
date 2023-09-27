81b2cbe29cb6104ab3f800701079912ef61a31e8
tbender2021 <143292915+tbender2021@users.noreply.github.com>
2023-09-26 20:28:16 -0400

    Initial commit

---
+++ lab-task-squirrel/Models/Task.swift
@@ -0,0 +1,42 @@
+//
+//  Task.swift
+//  lab-task-squirrel
+//
+//  Created by Charlie Hieger on 11/15/22.
+//
+
+import UIKit
+import CoreLocation
+
+class Task {
+    let title: String
+    let description: String
+    var image: UIImage?
+    var imageLocation: CLLocation?
+    var isComplete: Bool {
+        image != nil
+    }
+
+    init(title: String, description: String) {
+        self.title = title
+        self.description = description
+    }
+
+    func set(_ image: UIImage, with location: CLLocation) {
+        self.image = image
+        self.imageLocation = location
+    }
+}
+
+extension Task {
+    static var mockedTasks: [Task] {
+        return [
+            Task(title: "A Cool Flower",
+                 description: "Take a photo of a flower you think is pretty!"),
+            Task(title: "Bird",
+                 description: "Find a bird in your neighborhood!"),
+            Task(title: "Something Purple",
+                 description: "Find something in your neighborhood that is purple!")
+        ]
+    }
+}
